266281093eac9351c808f102915c7d6118f40aee
funkyblues <funkyblues@naver.com>
2022-01-09 16:14:18 +0900

    PUG COMPILATION - Pug Task part One - 2

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,10 +4,12 @@ import pug from "gulp-pug";
 const routes = {
 	pug: {
 		// src내부 폴더안에 있는 파일들은 컴파일 원하지 않음.
-		src: "src/*.pug"
+		src: "src/*.pug",
+		dest: "build"
+		
 	}
 }
-
-export const pug = () => gulp.src(routes.pug.src);
+// function 안에 pipe를 만든다. 그후 처리한 파일 저장 destination 추가.
+export const pug = () => gulp.src(routes.pug.src).pipe(pug()).pipe(gulp.dest());
 
 export const dev = () => console.log("I will dev");
